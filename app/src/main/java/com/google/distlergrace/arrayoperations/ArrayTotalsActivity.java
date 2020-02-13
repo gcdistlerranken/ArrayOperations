5753cd982bfd7ea936036c43f13ab9888a444670
grace_distler <grace_distler@insideranken.org>
2020-02-13 16:26:23 -0600

    Completed Array Operations App

--- app/src/main/java/com/google/distlergrace/arrayoperations/ArrayTotalsActivity.java
+++ app/src/main/java/com/google/distlergrace/arrayoperations/ArrayTotalsActivity.java
@@ -2,6 +2,7 @@ package com.google.distlergrace.arrayoperations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,13 @@ public class ArrayTotalsActivity extends AppCompatActivity
 {
     TextView    textViewTotalsResult;
     Button      buttonReturnHome;
+
+    Integer     sum;
+    Integer     average;
+    Integer     largestNumber;
+    Integer     smallestNumber;
+    Integer     range;
+    String      result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,10 +30,44 @@ public class ArrayTotalsActivity extends AppCompatActivity
         buttonReturnHome        = findViewById(R.id.buttonReturnHome);
 
         //Get The Intent In The Target Activity
+        Intent totalsIntent = getIntent();
 
         //Get The Attached Bundle From The Intent
+        Bundle extras = totalsIntent.getExtras();
 
         //Extract The Stored Data From The Bundle
+        if (extras != null)
+        {
+            if (extras.containsKey("sum"))
+            {
+                sum = extras.getInt("sum", 0);
+            }
+            if (extras.containsKey("average"))
+            {
+                average = extras.getInt("average", 0);
+            }
+            if (extras.containsKey("largestNumber"))
+            {
+                largestNumber = extras.getInt("largestNumber", 0);
+            }
+            if (extras.containsKey("smallestNumber"))
+            {
+                smallestNumber = extras.getInt("smallestNumber", 0);
+            }
+            if (extras.containsKey("range"))
+            {
+                range = extras.getInt("range", 0);
+            }
+
+            result  = "\nSum of 25 array elements: \t"  + sum.toString()            + "\n";
+            result += "\nAvg of 25 array elements: \t"  + average.toString()        + "\n";
+            result += "\nLargest array elements: \t"    + largestNumber.toString()  + "\n";
+            result += "\nSmallest array elements: \t"   + smallestNumber.toString() + "\n";
+            result += "\nRange of array elements: \t"   + average.toString()        + "\n";
+
+            textViewTotalsResult.setText(result);
+
+        }
 
         buttonReturnHome.setOnClickListener(new View.OnClickListener()
         {
--- app/src/main/java/com/google/distlergrace/arrayoperations/MainActivity.java
+++ app/src/main/java/com/google/distlergrace/arrayoperations/MainActivity.java
@@ -2,10 +2,16 @@ package com.google.distlergrace.arrayoperations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -21,7 +27,12 @@ public class MainActivity extends AppCompatActivity
     Button      buttonTotals;
 
     //Program Non-Widget Variables
-
+    ArrayList<Integer> integerList;
+    int     sum             = 0;
+    double  average         = 0;
+    int     largestNumber   = 0;
+    int     smallestNumber  = 0;
+    int     range           = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,13 +46,148 @@ public class MainActivity extends AppCompatActivity
         buttonSortDescending    = findViewById(R.id.buttonSortDescending);
         buttonTotals            = findViewById(R.id.buttonTotals);
 
+
         buttonNewNumbers.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                produceNewNumbers();
             }
         });
+
+        buttonSortAscending.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                clearAll();
+
+                //Sort Array List Into Ascending Order
+                Collections.sort(integerList);
+
+                for (int i = 0; i < 25; i++)
+                {
+                    textViewResults.setText(textViewResults.getText() + "" +
+                                            integerList.get(i) + ", ");
+                }
+            }
+        });
+
+        buttonSortDescending.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                clearAll();
+
+                //Sort Array List Into Descending Order
+                Collections.reverse(integerList);
+
+                for (int i = 0; i < 25; i++)
+                {
+                    textViewResults.setText(textViewResults.getText() + "" +
+                                            integerList.get(i) + ", ");
+                }
+            }
+        });
+
+        buttonTotals.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                sumArrayNums();
+                avgArrayNums();
+                largestArrayNum();
+                smallestArrayNum();
+                rangeArrayNums();
+
+                //Create A Bundle Object
+                Bundle extras = new Bundle();
+
+                //Add 'sum', 'average', 'largestNumber', 'smallestNumber', & 'range' To Bundle
+                extras.putInt("sum", sum);
+                extras.putDouble("average", average);
+                extras.putInt("largestNumber", largestNumber);
+                extras.putInt("smallestNumber", smallestNumber);
+                extras.putInt("range", range);
+
+                //Create & Initialize Intent
+                Intent totalsIntent =  new Intent(getApplicationContext(),
+                                                  ArrayTotalsActivity.class);
+
+                //Attach Bundle To Intent
+                totalsIntent.putExtras(extras);
+
+                //Start The Activity
+                startActivity(totalsIntent);
+            }
+        });
+    }
+
+    private ArrayList<Integer> produceNewNumbers()
+    {
+        clearNumberList();
+        clearAll();
+
+        integerList = new ArrayList<Integer>(25);
+        for (int i=1; i<101; i++)
+        {
+            integerList.add(new Integer(i));
+        }
+        Collections.shuffle(integerList);
+        for (int i=0; i<25; i++)
+        {
+            textViewResults.setText(textViewResults.getText() + "" + integerList.get(i) + ", ");
+        }
+
+        return integerList;
+    }
+
+    private void clearAll()
+    {
+        textViewResults.setText("");
+    }
+
+    private void clearNumberList()
+    {
+        integerList     = null;
+        sum             = 0;
+        average         = 0;
+        largestNumber   = 0;
+        smallestNumber  = 0;
+        range           = 0;
+    }
+
+    private int sumArrayNums()
+    {
+        sum = 0;
+
+        for(int i=0; i<integerList.size(); i++)
+        {
+            sum += integerList.get(i);
+        }
+        return sum;
+    }
+    private void avgArrayNums()
+    {
+        average = sum /integerList.size();
+    }
+
+    private int largestArrayNum()
+    {
+        largestNumber = Collections.max(integerList);
+        return largestNumber;
+    }
+
+    private int smallestArrayNum()
+    {
+        smallestNumber = Collections.min(integerList);
+        return smallestNumber;
+    }
+
+    private void rangeArrayNums()
+    {
+        range = largestNumber - smallestNumber;
     }
 }
